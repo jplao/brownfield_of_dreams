eb4056e1888f03c0820066802ca176b195bc5337
mmbensalah <mmbensalah@gmail.com>
2018-12-04 11:46:41 -0700

    added expect lines for name and url

--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe 'Repository' do
   it 'should be a class' do
     repo = Repository.new({name: "Greenhouse", url: "https://github.com/"})
     expect(repo).to be_an_instance_of(Repository)
+    expect(repo.name).to eq("Greenhouse")
+    expect(repo.url).to eq("https://github.com/")
   end
 end
