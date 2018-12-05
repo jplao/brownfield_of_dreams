2835f15f2cdd16294fb49a1385ecc5e824f4ef14
mmbensalah <mmbensalah@gmail.com>
2018-12-05 11:16:20 -0700

    adds before(:each) block

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
 
 describe 'Github API' do
-  it 'exists' do
+  before(:each) do
     user = create(:user, token: ENV["GITHUB_TOKEN"])
-    service = GithubService.new(user.token)
-    expect(service).to be_an_instance_of(GithubService)
+    @service = GithubService.new(user.token)
+  end
+
+  it 'exists' do
+    expect(@service).to be_an_instance_of(GithubService)
   end
 
   it 'returns repository links associated with a user' do
     VCR.use_cassette("GithubService_repos") do
-      user = create(:user, token: ENV["GITHUB_TOKEN"])
-      service = GithubService.new(user.token)
-      repo_list = service.get_repositories
+      repo_list = @service.get_repositories
 
       expect(repo_list.class).to eq(Array)
       expect(repo_list[0].keys).to include(:name)
