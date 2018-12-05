ced8e3d7afe4244f90a742a8f2dd7195161b5442
mmbensalah <mmbensalah@gmail.com>
2018-12-05 06:57:12 -0700

    moves DatabaseCleaner configure block from spec helper to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'database_cleaner'
 require 'vcr'
 require 'webmock/rspec'
 
@@ -15,7 +16,6 @@ VCR.configure do |config|
   config.configure_rspec_metadata!
   config.filter_sensitive_data("<YOUTUBE_API_KEY>") { ENV['YOUTUBE_API_KEY'] }
 end
-
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -48,4 +48,24 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+
+  config.before(:suite) do
+     DatabaseCleaner.clean_with(:truncation)
+   end
+
+   config.before(:each) do
+     DatabaseCleaner.strategy = :transaction
+   end
+
+   config.before(:each, :js => true) do
+     DatabaseCleaner.strategy = :truncation
+   end
+
+   config.before(:each) do
+     DatabaseCleaner.start
+   end
+
+   config.after(:each) do
+     DatabaseCleaner.clean
+   end
 end
