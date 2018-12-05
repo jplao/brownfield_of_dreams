785711c9e644057f46d13f782dcd31126f16659c
mmbensalah <mmbensalah@gmail.com>
2018-12-05 11:31:47 -0700

    changes routes for user logging to dashboard controller#show

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
 
-  get '/dashboard', to: 'users#show'
+  get '/dashboard', to: 'dashboard#show'
   get '/about', to: 'about#show'
   get '/get_started', to: 'get_started#show'
 
