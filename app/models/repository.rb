145e54d37aaa2548fd7c084e7586331ca433f5fc
mmbensalah <mmbensalah@gmail.com>
2018-12-05 06:59:17 -0700

    creates Repository model

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,9 +1,9 @@
 class Repository
   attr_reader :name,
               :url
-              
-  def initialize(hash)
-    @name = hash[:name]
-    @url  = hash[:url]
+
+  def initialize(repo_data)
+    @name = repo_data[:name]
+    @url  = repo_data[:url]
   end
 end
