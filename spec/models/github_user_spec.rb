112ce3e9d621d9b2788e9910dd5fd2d48517babb
mmbensalah <mmbensalah@gmail.com>
2018-12-05 21:47:59 -0700

    edited test

--- spec/models/github_user_spec.rb
+++ spec/models/github_user_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'Github User' do
   it 'exists' do
-    name = 'name'
-    url  = 'http://name.com'
-    user = GithubUser.new(name, url)
+    data = {login: 'name', url: 'http://name.com'}
+    user = GithubUser.new(data)
 
     expect(user).to be_an_instance_of(GithubUser)
-    expect(user.name).to eq(name)
-    expect(user.url).to eq(url)
+    expect(user.login).to eq('name')
+    expect(user.url).to eq('http://name.com')
   end
 end
