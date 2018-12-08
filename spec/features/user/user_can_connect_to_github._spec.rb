6a3102b635150dd0300ab83e51dfdd2ed6d73bce
Jenn Lao <jenn.p.lao@gmail.com>
2018-12-08 12:33:53 -0700

    adds more expectations

--- spec/features/user/user_can_connect_to_github._spec.rb
+++ spec/features/user/user_can_connect_to_github._spec.rb
@@ -8,6 +8,10 @@ RSpec.describe 'User can connect to Github' do
     fill_in :session_password, with: user.password
     click_on 'Log In'
 
+    expect(current_path).to eq dashboard_path
     expect(page).to have_link('Connect to GitHub')
+    expect(page).to_not have_content('Repositories')
+    expect(page).to_not have_content('Following')
+    expect(page).to_not have_content('Followers')
   end
 end
