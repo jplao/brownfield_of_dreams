df50c9079791afa74af5c923eb5d1945f2560c18
Jenn Lao <jenn.p.lao@gmail.com>
2018-12-10 14:32:01 -0700

    creates test where user only sees classroom tutorials if logged in

---
+++ spec/features/user/user_can_see_classroom_content_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'User can see tutorials' do
+  it 'can only see classroom content when logged in' do
+    tutorial_1, tutorial_2 = create_list(:tutorial, 2)
+    tutorial_3, tutorial_4 = create_list(:tutorial, 2, classroom: true)
+
+    visit root_path
+
+    expect(page).to have_content(tutorial_1.title)
+    expect(page).to have_content(tutorial_2.title)
+
+    expect(page).not_to have_content(tutorial_3.title)
+    expect(page).not_to have_content(tutorial_4.title)
+
+    user_1 = create(:user)
+
+    visit login_path
+    fill_in :session_email,    with: user_1.email
+    fill_in :session_password, with: user_1.password
+    click_on 'Log In'
+
+    visit root_path
+    expect(page).to have_content(tutorial_1.title)
+    expect(page).to have_content(tutorial_2.title)
+    expect(page).to have_content(tutorial_3.title)
+    expect(page).to have_content(tutorial_4.title)
+  end
+end
