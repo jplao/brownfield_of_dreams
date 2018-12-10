2ca4f1af61df7586bf210c1ca235d38a6f8c9c48
Jenn Lao <jenn.p.lao@gmail.com>
2018-12-10 14:30:34 -0700

    adds another conditional to only show all tutorials if logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,10 @@ class WelcomeController < ApplicationController
   def index
     if params[:tag]
       @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
-    else
+    elsif current_user
       @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
+    else
+      @tutorials = Tutorial.where(classroom: false).paginate(:page => params[:page], :per_page => 5)
     end
   end
 end
