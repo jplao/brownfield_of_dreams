0f8dcb2393ffb6562389e038c3965b307089be45
mmbensalah <mmbensalah@gmail.com>
2018-12-12 16:09:13 -0700

    config for production.rb for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,17 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.action_mailer.delivery_method = :smtp
 
+      config.action_mailer.smtp_settings = {
+        address:              'smtp.sendgrid.net',
+        port:                 '587',
+        domain:               'https://sheltered-sands-76845.herokuapp.com/',
+        user_name:            ENV["SENDGRID_USERNAME"],
+        password:             ENV["SENDGRID_PASSWORD"],
+        authentication:       'plain',
+        enable_starttls_auto: true
+      }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
