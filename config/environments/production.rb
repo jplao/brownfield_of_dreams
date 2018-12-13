91e192aa7792ad464c172eb2d136735856c9761c
mmbensalah <mmbensalah@gmail.com>
2018-12-13 15:23:08 -0700

    adds raise delivery errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'sheltered-sands-76845.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
