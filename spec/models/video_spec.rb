72d23b6e555baf10165351eadef46618261670fd
mmbensalah <mmbensalah@gmail.com>
2018-12-13 12:19:19 -0700

    creates test to test max position method

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,3 +1,4 @@
+require 'rails_helper'
 RSpec.describe Video, type: :model do
   describe 'validations' do
     it {should validate_presence_of(:position)}
@@ -7,5 +8,17 @@ RSpec.describe Video, type: :model do
     it {should have_many :user_videos}
     it {should have_many(:users).through(:user_videos)}
     it {should belong_to :tutorial}
+  end
+
+  describe 'class method' do
+    it '#max_position' do
+      tutorial = create(:tutorial)
+      video    = create(:video, tutorial: tutorial)
+      video_1  = create(:video, tutorial: tutorial, position: 1)
+      video_2  = create(:video, tutorial: tutorial, position: 2)
+      video.update_attribute(:position, nil)
+
+      expect(video.max_position).to eq(3)
+    end
   end
 end
