1146fbc76e0f88ee7290c97b857b2e7b013fde31
Jenn Lao <jenn.p.lao@gmail.com>
2018-12-13 14:59:06 -0700

    removes second config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,20 +4,6 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
-  config.action_mailer.delivery_method = :smtp
-  host = 'serene-depths-68097.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-      config.action_mailer.smtp_settings = {
-        address:              'smtp.sendgrid.net',
-        port:                 '587',
-        domain:               'heroku.com',
-        user_name:            ENV["SENDGRID_USERNAME"],
-        password:             ENV["SENDGRID_PASSWORD"],
-        authentication:       'plain',
-        enable_starttls_auto: true
-      }
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
