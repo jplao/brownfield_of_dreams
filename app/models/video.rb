a16a799a13ad0d7943c63dfaf16e1bd3a30c3545
mmbensalah <mmbensalah@gmail.com>
2018-12-13 12:18:37 -0700

    creates max position method

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,9 @@ class Video < ApplicationRecord
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial
-  validates_presence_of :position
+  validates_presence_of :position, on: :create
 
+  def max_position
+    tutorial.videos.maximum("position") + 1
+  end
 end
