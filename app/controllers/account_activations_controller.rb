26bd3fe91d65fdba3d0365b142d8876412fb3f6d
Jenn Lao <jenn.p.lao@gmail.com>
2018-12-12 18:52:37 -0700

    fixes redirect

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,9 +3,8 @@ class AccountActivationsController < ApplicationController
    user = User.find_by(email: params[:email])
    if user && !user.activated? && user.authenticated?('activation', params[:id])
      user.activate
-     log_in user
      flash[:success] = "Account activated!"
-     redirect_to dashboard_path
+     redirect_to '/dashboard'
    else
      flash[:danger] = "Invalid activation link"
      redirect_to root_url
