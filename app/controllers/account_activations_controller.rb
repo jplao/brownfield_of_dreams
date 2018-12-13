d73b8f3715c6d755ad7c16c965a4794fe324241c
Jenn Lao <jenn.p.lao@gmail.com>
2018-12-12 19:21:00 -0700

    defines log_in method

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,15 +1,14 @@
 class AccountActivationsController < ApplicationController
   def edit
-    @user = User.find_by(email: params[:email])
-    @current_user = @user
-    if @user && !@user.activated? && @user.authenticated?(params[:id])
-      @user.activate
-      @user.update_attribute(:activated,    true)
-      @user.update_attribute(:activated_at, Time.zone.now)
-      flash[:notice] = "Thank you! Your account is now activated."
-      redirect_to '/dashboard'
-    else
-      render file: '/public/404'
-    end
-  end
+   user = User.find_by(email: params[:email])
+   if user && !user.activated? && user.authenticated?('activation', params[:id])
+     user.activate
+     log_in user
+     flash[:success] = "Account activated!"
+     redirect_to login_path
+   else
+     flash[:danger] = "Invalid activation link"
+     redirect_to root_url
+   end
+ end
 end
