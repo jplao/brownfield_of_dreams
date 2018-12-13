9f090bb139a2bbf3f1fe3304f73db6c592a3bdff
Jenn Lao <jenn.p.lao@gmail.com>
2018-12-12 18:43:28 -0700

    changes fill-in

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,7 +46,7 @@ class User < ApplicationRecord
   end
 
   def activate
-    update_columns(activated: FILL_IN, activated_at: FILL_IN)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   def send_activation_email
